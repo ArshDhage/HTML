5b4fdddae9a4d2518511c45db7e6c15cd959e65d
ArshDhage <arshdhage23@gmail.com>
2026-01-27 20:07:03 +0530

    Add files via upload

---
+++ arsh_dhage/javaScript/array/27-1-26-one.js
@@ -0,0 +1,20 @@
+// ARRAY
+
+// CREATE ARRAY
+let numbers = [10, 20, 30, 40]
+// length: 1, 2, 3, 4
+//         [__,__,__,__]
+// index:  0, 1, 2, 3
+
+let strings = ["string1","string2"]
+let answers = [true, false]
+
+// ARRAY is collection of similar data types
+
+let array = ["string", 10]
+
+// ARRAY READ
+console.log(numbers)
+
+console.log(numbers[0])
+console.log(typeof(array))
