c2be4a7c8c51d78cfb9556fcb1ba8362d2f2f198
ArshDhage <arshdhage23@gmail.com>
2026-02-23 20:18:34 +0530

    Add files via upload

---
+++ java_class/classOne.java
@@ -0,0 +1,21 @@
+public class classOne{
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+
+        int number = 18;
+        System.out.println(number);
+
+        char letter = 'v';
+        System.out.println(letter);
+
+        short num = 1818;
+        System.out.println(num);
+
+        byte n = 101;
+        System.out.println(n);
+
+        long longNumber = 478104186433l;
+        System.out.println(longNumber);
+
+    }
+}
